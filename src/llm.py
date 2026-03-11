de3b43395aaa705b486cf52af4547a0407710860
Raphael Resende <raphael.resende.business@gmail.com>
2026-03-11 19:28:40 -0300

    feat: implement run_agent function using LangChain and Ollama for transcript processing

---
+++ src/llm.py
@@ -0,0 +1,14 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+def run_agent(transcript: str, role_prompt: str):
+    llm = ChatOllama(model= "llama3.2:3b", temperature= 0.2)
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", role_prompt),
+        ("user", "Texto da Reunião: {text}")
+    ])
+
+    chain = prompt | llm | StrOutputParser
+    return chain.invoke({"text": transcript}).content
+
